0190e427b9fe82184d2e1ed3574eb58a8370b8c1
PugMau <166648867+PugMau@users.noreply.github.com>
2024-07-11 02:00:50 +0000

    Cambio Final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+import todo from "../../img/image.jpg";
+import "../../styles/index.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [tasks, setTasks] = useState([
+		{ id: 1, title: "Make the bed" },
+		{ id: 2, title: "Wash my hands" },
+		{ id: 3, title: "Eat" },
+		{ id: 4, title: "Walk the dog" }
+	]);
+
+	const backgroundStyle = {
+		backgroundImage: `url(${todo})`,
+		backgroundSize: "cover",
+		backgroundPosition: "center",
+		backgroundRepeat: "no-repeat",
+		height: "100vh",
+		margin: 0,
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+	};
+
+	const cardStyle = {
+		width: "30rem",
+	};
+
+	const handleKeyDown = (e) => {
+		if (e.key === "Enter") {
+			const value = e.target.value.trim();
+			if (value !== "") {
+				const newTask = {
+					id: tasks.length + 1,
+					title: value
+				};
+				setTasks([...tasks, newTask]);
+				e.target.value = "";
+			}
+		}
+	};
+
+	const handleDeleteTask = (id) => {
+		const updatedTasks = tasks.filter(task => task.id !== id);
+		setTasks(updatedTasks);
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div style={backgroundStyle}>
+			<div className="container">
+				<h1>To Do List!</h1>
+			</div>
+			<div className="card" style={cardStyle}>
+				<ul className="list-group list-group-flush">
+					<li className="list-group-item list-group-item-input">
+						<i className="fa-solid fa-list"></i>
+						{tasks.length === 0 ? (
+							<input
+								type="text"
+								placeholder="No tasks, add tasks"
+								onKeyDown={handleKeyDown}
+							/>
+						) : (
+							<input
+								type="text"
+								placeholder="What's need to be done?"
+								onKeyDown={handleKeyDown}
+							/>
+						)}
+					</li>
+					{tasks.length === 0 ? (
+						<li className="list-group-item">
+							<div className="list-item-content">No tasks, add tasks</div>
+						</li>
+					) : (
+						tasks.map(task => (
+							<li className="list-group-item" key={task.id}>
+								<i className="fa-regular fa-square-check"></i>
+								<div className="list-item-content">{task.title}</div>
+								<button
+									type="button"
+									className="btn-close"
+									aria-label="Close"
+									onClick={() => handleDeleteTask(task.id)}
+								></button>
+							</li>
+						))
+					)}
+				</ul>
+				<div className="card-footer">
+					<i className="fa-solid fa-triangle-exclamation"></i>
+					{tasks.length} {tasks.length === 1 ? "item" : "items"} left
+				</div>
+			</div>
 		</div>
 	);
 };
